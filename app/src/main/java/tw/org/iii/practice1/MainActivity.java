6bb37e3ad59dc28779f36915adc80aeab37a7eee
sandylu66 <sandy66.lu@gmail.com>
2020-08-04 17:35:38 +0800

    Initial commit

---
+++ app/src/main/java/tw/org/iii/practice1/MainActivity.java
@@ -0,0 +1,36 @@
+package tw.org.iii.practice1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+      Intent mIntent;
+      switch (v.getId())
+      {
+          case R.id.btn1:
+              break;
+          case R.id.btn2:
+              finish();
+              break;
+      }
+
+    }
+}
